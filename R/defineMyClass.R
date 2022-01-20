e1f8ddf6c25754ca7fa9394f1873e3c26624b0f4
chan <790371816@qq.com>
2022-01-20 16:22:54 +0800

    Amend defind class bug when load this package.

--- R/defineMyClass.R
+++ R/defineMyClass.R
@@ -1,6 +1,8 @@
 #' @title A function to define how the data store in the findmyCL object
 #' @export
 defineMyClass <- function(){
+  class_cache <- new.env(parent = emptyenv())
+  #生成一个全局的环境，用于存放设置的类
   setClass(Class = "findmyCLclass",
            slots = list(path = "character",
                         file = "character",
@@ -11,5 +13,6 @@ defineMyClass <- function(){
                         ms2CheckResult = "list",
                         noMS2 = "list",
                         ms2MatchResult = "list"),
-           package = "findmyCL")
+           where = class_cache)
+  #where指的是环境的位置
 }
